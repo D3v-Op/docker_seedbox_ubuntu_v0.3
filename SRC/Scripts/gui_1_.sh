18e4b2ae9968f832a3a0ab82ef64ef69335992e3
MOOGAA <arjunsinhchudasama7.6@gmail.com>
2021-05-21 09:04:08 +0530

    Update gui_1_.sh

--- SRC/Scripts/gui_1_.sh
+++ SRC/Scripts/gui_1_.sh
@@ -18,11 +18,20 @@ echo
 
 apt-get -qq update && \
 wget \
-  https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && \
+  https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb \
+    -O /path/ && \
+cd /path/ && \
 dpkg --install chrome-remote-desktop_current_amd64.deb && \
 apt-get -qqy install --fix-broken && \
-apt-get -qq update
-
+apt-get -qq update && \
+DEBIAN_FRONTEND=noninteractive \
+apt-get -qqy install \
+  xfce4 \
+  desktop-base && \
+bash -c \
+  'echo \
+    "exec /etc/X11/Xsession /usr/bin/xfce4-session" \
+      > /etc/chrome-remote-desktop-session'
 
 
 ###########################################################################################################################################################################################################################################################################################################################################################################################
