c416bb3fd3f1566e3b60cb5b9e13db092e88e6aa
MOOGAA <arjunsinhchudasama7.6@gmail.com>
2021-05-21 08:23:40 +0530

    Update setup_.sh

--- SRC/setup_.sh
+++ SRC/setup_.sh
@@ -1,1 +1,78 @@
-123
+#!/bin/bash
+
+
+
+###########################################################################################################################################################################################################################################################################################################################################################################################
+
+
+
+echo ; \
+echo ; \
+echo ; \
+echo ' ⟩   Running setup_.sh ... ' ; \
+echo ; \
+echo ; \
+echo
+
+echo ; \
+echo ; \
+echo ; \
+echo ' ⟩   Running Update , Upgrade , Autoremove & Fixing Missing / Broken ... ' ; \
+echo ; \
+echo ; \
+echo
+
+apt-get -qq update && \
+apt-get -qqy autoremove && \
+apt-get -qq update && \
+apt-get -qqy upgrade && \
+apt-get -qq update && \
+apt-get -qqy install --fix-missing --fix-broken && \
+apt-get -qq update
+###
+
+
+
+echo ; \
+echo ; \
+echo ; \
+echo ' ⟩  Installing Core / IMP* Libs ... ' ; \
+echo ; \
+echo ; \
+echo
+
+apt-get -qqy install \
+      curl \
+      htop \
+      iputils-ping \
+      iptables \
+      jq \
+      nano \
+      sudo \
+      systemctl \
+      ufw \
+      wget && \
+apt-get -qq update
+###
+
+
+
+echo ; \
+echo ; \
+echo ; \
+echo ' ⟩   Installing Required Dependency Libs ... ' ; \
+echo ; \
+echo ; \
+echo
+
+apt-get -qqy install \
+      build-essential \
+      docker.io \
+      make \
+      npm \
+      python3.9 \
+      python3-pip \
+      software-properties-common \
+      tzdata ; \
+apt-get -qq update
+###
